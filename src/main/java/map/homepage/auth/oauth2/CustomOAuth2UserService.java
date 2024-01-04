ebfe2ae3860650cb7fa12690c0734d84381bd3f0
wuseong <jws001228@naver.com>
2024-01-04 23:52:55 +0900

    fix: loadUser후 발생하는 버그 해결
    - 반환 인증객체를 생성할때 Attributes와 식별 key를 적절한 것으로 바꾸도록 수정
    - profile_imageUri, provider, oauthId도 DB에 잘 저장할 수 있도록 변경

--- src/main/java/map/homepage/auth/oauth2/CustomOAuth2UserService.java
+++ src/main/java/map/homepage/auth/oauth2/CustomOAuth2UserService.java
@@ -1,56 +1,52 @@
 package map.homepage.auth.oauth2;
 
 import lombok.RequiredArgsConstructor;
-import map.homepage.domain.user.User;
-import map.homepage.domain.user.UserRepository;
+import map.homepage.domain.member.Member;
+import map.homepage.domain.member.MemberRepository;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.security.Provider;
 import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
-    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
     //1. 인증이 완료 됐으면 코드를 통해 토큰을 가져오고 토큰을 통해 유저 정보를 가져오기
     //2. 가져온 유저정보를 DB에 저장 혹은 갱신
     @Override
     public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) {
         OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
 
-        //Provider와 사용자 이름을 가져옴
+        // Provider와 사용자 이름을 가져옴
         String provider = oAuth2UserRequest.getClientRegistration().getRegistrationId();
-        String oauthId = oAuth2UserRequest.getClientRegistration().getProviderDetails()
-                .getUserInfoEndpoint().getUserNameAttributeName();
-        OAuthAttributes attributes = OAuthAttributes.of(provider, oauthId, oAuth2User.getAttributes());
+        String userNameAttributeName = oAuth2UserRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        // oauth2 플랫폼 마다 응답 형식이 다르므로 구분해서 attribute를 구성
+        OAuthAttributes attributes = OAuthAttributes.of(provider, oAuth2User.getAttributes());
 
         //유저정보와 provider를 넘겨 db에 저장시킨다
-        User user = saveOrUpdate(attributes, oauthId);
+        System.out.println("oAuth2User = " + userNameAttributeName);
+        Member member = saveOrUpdate(attributes, oAuth2User.getName());
 
         // Security Context에 저장되는 유저객체
         return new DefaultOAuth2User(
-                Collections.singleton(new SimpleGrantedAuthority(user.getRole().toString())),
-                attributes.getAttributes(),
-                oauthId
+                //구글은 "sub" 라는 명확한 String value를, naver는 "response"내에 객체로 unique를 식별
+                Collections.singleton(new SimpleGrantedAuthority(member.getRole().toString())),
+                oAuth2User.getAttributes(),
+                userNameAttributeName
         );
     }
 
-    private User saveOrUpdate(OAuthAttributes attributes, String oauthId) {
-        User user = userRepository.findByOauthId(oauthId).orElseGet( () ->{
-            User newUser = attributes.toEntity();
-            newUser.setOauthId(oauthId);
-            return userRepository.save(newUser);
+    private Member saveOrUpdate(OAuthAttributes attributes, String oauthId) {
+        Member member = memberRepository.findByOauthId(oauthId).orElseGet( () ->{
+            Member newMember = attributes.toEntity();
+            return memberRepository.save(newMember);
         });
-        user.update(attributes.getName(), attributes.getEmail(), attributes.getImageUri());
-        return userRepository.save(user);
+        member.update(attributes.getName(), attributes.getEmail(), attributes.getImageUri());
+        return memberRepository.save(member);
     }
 }
