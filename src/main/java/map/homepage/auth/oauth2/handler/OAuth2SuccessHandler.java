96eb5ce91f7ac43d405ff239bb6520a2bb9e0f56
wuseong <jws001228@naver.com>
2024-01-08 20:08:20 +0900

    feat: 인증 성공 후 jwt 토큰 응답에 추가
    - 인증 객체를 통해 사용자 정보를 가져와 토큰 생성 및 응답 헤더에 추가

--- src/main/java/map/homepage/auth/oauth2/handler/OAuth2SuccessHandler.java
+++ src/main/java/map/homepage/auth/oauth2/handler/OAuth2SuccessHandler.java
@@ -4,22 +4,51 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import map.homepage.auth.jwt.service.JwtUtil;
+import map.homepage.auth.jwt.token.JwtToken;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 
-
+/*
+ jwt 토큰을 만들어 응답 정보에 추가 해주기
+ */
+@Slf4j
 @Component
-@NoArgsConstructor
-public class OAuth2SuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler{
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private final JwtUtil jwtUtil;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
-        super.onAuthenticationSuccess(request, response, authentication);
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
+        // OAuth2User로 캐스팅하여 인증된 사용자 정보를 가져온다.
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        // 사용자 이메일을 가져온다.
+        String email = oAuth2User.getAttribute("email");
+        // OAuth2User로 부터 Role을 얻어온다.
+        String role = oAuth2User.getAuthorities().stream().
+                findFirst() // 첫번째 Role을 찾아온다.
+                .orElseThrow(IllegalAccessError::new) // 존재하지 않을 시 예외를 던진다.
+                .getAuthority(); // Role을 가져온다.
+        log.info("role = {}", role);
+        // 회원이 존재하면 jwt token 발행을 시작한다.
+        JwtToken token = jwtUtil.generateToken(email, role);
+        log.info("requesturI = {}", request.getRequestURI());
+        log.info("accessToken = {}", token.getAccessToken());
+        response.addHeader("Access-Token", token.getAccessToken());
+        response.addHeader("Refresh-Token", token.getRefreshToken());
     }
+
 }
