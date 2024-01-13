8ef0af9daa09e91c3d047a5105f39a19f8190469
jimizip <rnwlals02@naver.com>
2024-01-14 03:46:09 +0900

    merge: post 최근 병합 이슈 해결

--- src/main/java/map/homepage/domain/post/Post.java
+++ src/main/java/map/homepage/domain/post/Post.java
@@ -4,34 +4,47 @@ import jakarta.persistence.*;
 import lombok.*;
 import map.homepage.domain.member.Member;
 import map.homepage.domain.member.Role;
+import map.homepage.domain.post.comment.Comment;
+
+import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
 
     @Id // 기본키
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본키 자동 생성
+    @GeneratedValue(strategy = GenerationType.AUTO) //기본키 자동 생성
     private Long id; // 게시글 ID
 
-    // 아직 매핑 불충분해서 주석처리
-    //@JoinColumn(name = "member_id") // 외래키
-    //private Member member;
+    @ManyToOne(fetch = FetchType.LAZY) // 지연 로딩
+    @JoinColumn // 외래키
+    private Member member; // 게시글 작성자
 
-    // 필수
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
+
     private String title; // 게시글 제목
     private String content; // 게시글 내용
     private String dtype; // 게시글 타입
+    private int views; // 게시글 조회 수
 
-    @Enumerated(EnumType.STRING) //Role에서 enum으로 가져옴
+    @Enumerated(EnumType.STRING) // Role에서 enum으로 가져옴
     private Role role; // 읽기 권한
 
-    // 선택
-    @Lob
-    private byte[] fileData; // 첨부 파일
 
-    // 첨부파일 저장되면 썸네일을 생성하는 로직 필요
-    private String tuhmbnail; // 썸네일
+    /* 데이터 정합성을 위한 편의 메소드
+    public void addComment(Comment comment){
+        comments.add(comment);
+        comment.setPost(this);
+    }
+
+    public void removeComment(Comment comment){
+        comments.remove(comment);
+        comment.setPost(null);
+    }
+    */
 }
