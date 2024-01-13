fd11e028e08b6eb003ca8e84f00e66e4d179670a
jimizip <rnwlals02@naver.com>
2024-01-14 02:55:41 +0900

    feat: Comment 엔티티 매핑

---
+++ src/main/java/map/homepage/domain/common/BaseEntity.java
@@ -0,0 +1,22 @@
+package map.homepage.domain.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt; // 생성 시간
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt; // 수정 시간
+}
--- src/main/java/map/homepage/domain/post/comment/Comment.java
+++ src/main/java/map/homepage/domain/post/comment/Comment.java
@@ -1,4 +1,40 @@
 package map.homepage.domain.post.comment;
 
-public class Comment {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import map.homepage.domain.common.BaseEntity;
+import map.homepage.domain.member.Member;
+import map.homepage.domain.member.Role;
+import map.homepage.domain.post.Post;
+
+import java.time.LocalDateTime;
+
+@Entity // 해당 클래스가 JPA의 엔티티임을 명시
+@Builder // 빌더 패턴 (디자인 패턴)
+@Getter // getter 생성
+@NoArgsConstructor
+@AllArgsConstructor // 빌더 패턴 사용을 위해
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+
+    // 외래키
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    // 읽기 권한
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
