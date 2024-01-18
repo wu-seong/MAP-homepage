fe147761b9aaccc03fb85cdcf12c5871d57278e4
jimizip <rnwlals02@naver.com>
2024-01-18 15:34:20 +0900

    comment: 어노테이션 주석 추가

--- src/main/java/map/homepage/domain/post/comment/CommentController.java
+++ src/main/java/map/homepage/domain/post/comment/CommentController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자(Front-End) 와 백엔드를 연결해주는 역할
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api")
+@RestController // RestApi 사용 시
+@RequiredArgsConstructor // final이나 @NonNull인 필드 값만 파라미터로 받는 생성자 만듦
+@RequestMapping("/api") // 들어온 요청을 특정 method와 매핑하기 위해 사용
 public class CommentController {
     @GetMapping("/hello")
     public String hello(){
--- src/main/java/map/homepage/domain/post/comment/CommentDto.java
+++ src/main/java/map/homepage/domain/post/comment/CommentDto.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Data // getter, setter 만들어줌
+@NoArgsConstructor // 파라미터가 없는 기본 생성자를 생성
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 만듬
 public class CommentDto {
     private Long id;
     private String content;
