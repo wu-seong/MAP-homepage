ae746cadbf93cc089a4974dbc7df04922ee3c719
jimizip <rnwlals02@naver.com>
2024-01-20 04:31:40 +0900

    feat: 댓글 Controller, Service 기본 틀 작업

--- src/main/java/map/homepage/domain/post/comment/CommentController.java
+++ src/main/java/map/homepage/domain/post/comment/CommentController.java
@@ -1,17 +1,47 @@
 package map.homepage.domain.post.comment;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import map.homepage.domain.member.MemberRepository;
+import map.homepage.domain.post.comment.Dto.CommentCreateRequest;
+import map.homepage.domain.post.comment.Dto.CommentReadCondition;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 // 사용자(Front-End) 와 백엔드를 연결해주는 역할
 @RestController // RestApi 사용 시
 @RequiredArgsConstructor // final이나 @NonNull인 필드 값만 파라미터로 받는 생성자 만듦
 @RequestMapping("/api") // 들어온 요청을 특정 method와 매핑하기 위해 사용
 public class CommentController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello swagger";
+    private  CommentService commentService;
+    private  MemberRepository memberRepository;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    // 댓글 조회 (불러오기)
+    @ApiResponses
+    @GetMapping("/comment/{post_id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Response getComment(@Valid CommentReadCondition commentReadCondition) {
+        return new Response();
+    }
+
+    // 댓글 생성
+    @PostMapping("/comment/{post_id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Response writeComment(@Valid @RequestBody CommentCreateRequest commentCreateRequest, Authentication authentication) {
+        return new Response();
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/comment/{post_id}/{comment_id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Response deleteComment(@PathVariable("post_id") Long postId, @PathVariable("comment_id") Long commentId, Authentication authentication){
+        return new Response();
     }
 }
--- src/main/java/map/homepage/domain/post/comment/CommentService.java
+++ src/main/java/map/homepage/domain/post/comment/CommentService.java
@@ -1,10 +1,43 @@
 package map.homepage.domain.post.comment;
 
-import lombok.RequiredArgsConstructor;
+import jakarta.transaction.Transactional;
+import map.homepage.domain.post.PostRepository;
+import map.homepage.domain.post.comment.Dto.CommentCreateRequest;
+import map.homepage.domain.post.comment.Dto.CommentDto;
+import map.homepage.domain.post.comment.Dto.CommentReadCondition;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 // Repository에게 받은 정보를 가공해서 Controller에게 주거나, Controller에게 받은 정보를 Repository한테 주는 역할
-@RequiredArgsConstructor
+// @RequiredArgsConstructor
 @Service
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+    public CommentService(final CommentRepository commentRepository, final PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+
+    @Transactional
+    public List<CommentDto> getComment(final CommentReadCondition condition) {
+        return commentRepository.findAllByPostId(condition.getPostId()).stream()
+                .map(CommentDto::toDto)
+                .collect(Collectors.toList());
+    }
+
+    // 도하 오빠 작업 후
+    @Transactional
+    public CommentDto writeComment(final CommentCreateRequest commentCreateRequest) {
+       return null;
+    }
+
+    @Transactional
+    public void deleteComment(final Long id) {
+        Comment comment = commentRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("댓글 id를 찾을 수 없습니다."));
+        commentRepository.delete(comment);
+    }
+
 }
