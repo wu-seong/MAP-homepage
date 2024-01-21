0eaf11e3df8bd049dbab9288fa9b28730d6fb368
wuseong <jws001228@naver.com>
2024-01-22 01:13:37 +0900

    test: 테스트 API swagger에 추가

--- src/main/java/map/homepage/domain/member/auth/jwt/controller/TestController.java
+++ src/main/java/map/homepage/domain/member/auth/jwt/controller/TestController.java
@@ -1,6 +1,7 @@
 package map.homepage.domain.member.auth.jwt.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import map.homepage.domain.member.Member;
 import map.homepage.domain.member.auth.MemberContext;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class TestController {
+
+    @Operation(summary = "테스트 API",description = "토큰검증 후 파싱하여 유저 객체 전역적으로 생성하는지 테스트")
     @GetMapping("/test")
     public Member test(){
         Member member = MemberContext.getMember();
