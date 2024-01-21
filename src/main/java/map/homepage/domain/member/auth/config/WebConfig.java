9c0ed6d75097cff9a1c287b621de519302e9300e
wuseong <jws001228@naver.com>
2024-01-22 01:43:18 +0900

    feat: cors 설정 https 추가

--- src/main/java/map/homepage/domain/member/auth/config/WebConfig.java
+++ src/main/java/map/homepage/domain/member/auth/config/WebConfig.java
@@ -46,7 +46,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Access-Token", "Refresh-Token");
