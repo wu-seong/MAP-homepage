9a613c27c6fb3de7e51857c3b156b267449ea605
wuseong <jws001228@naver.com>
2024-01-22 11:19:45 +0900

    feat: corsFilter 생성 및 등록
    기존 Cors 제어방식은 서블릿 디스패처 수준에서 이루어지는데 인증을 처리하는JwtFilter가 이보다 앞서 있기 때문에 여기서 통과하지 못하면 cors처리를 못하였기 때문에 직접 필터를 만들어 JwtFilter앞에 등록

--- src/main/java/map/homepage/domain/member/auth/config/WebConfig.java
+++ src/main/java/map/homepage/domain/member/auth/config/WebConfig.java
@@ -2,6 +2,7 @@ package map.homepage.domain.member.auth.config;
 
 import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
+import map.homepage.domain.member.auth.jwt.filter.CorsFilter;
 import map.homepage.domain.member.auth.jwt.filter.JwtAuthFilter;
 import map.homepage.domain.member.auth.jwt.filter.MemberInterceptor;
 import map.homepage.domain.member.auth.jwt.service.JwtTokenProvider;
@@ -22,11 +23,21 @@ public class WebConfig implements WebMvcConfigurer {
     private final JwtUtil jwtUtil;
     private final MemberService memberService;
     private final EntityManagerFactory entityManagerFactory;
+
     @Bean
     public FilterRegistrationBean<JwtAuthFilter> jwtFilterRegistration() {
         FilterRegistrationBean<JwtAuthFilter> registration = new FilterRegistrationBean<>();
         registration.setFilter(new JwtAuthFilter(jwtTokenProvider)); // 필터 인스턴스 설정
-        registration.addUrlPatterns("/**");
+        registration.addUrlPatterns("/*");
+        registration.setOrder(2); // 필터의 순서 설정. 값이 낮을수록 먼저 실행
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
+        FilterRegistrationBean<CorsFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new CorsFilter()); // 필터 인스턴스 설정
+        registration.addUrlPatterns("/*");
         registration.setOrder(1); // 필터의 순서 설정. 값이 낮을수록 먼저 실행
         return registration;
     }
@@ -43,12 +54,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://localhost:3000")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .exposedHeaders("Access-Token", "Refresh-Token");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000", "https://localhost:3000")
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .exposedHeaders("Access-Token", "Refresh-Token");
+//    }
 }
