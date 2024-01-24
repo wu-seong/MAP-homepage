12b505eb94ed38217510eed3d77ff44dae77d9f2
kwondoha <ehehgk@naver.com>
2024-01-24 10:39:58 +0900

    rename: BaseEntity의 setter 삭제 및 LocalDateTime으로 변경

--- src/main/java/map/homepage/domain/common/BaseEntity.java
+++ src/main/java/map/homepage/domain/common/BaseEntity.java
@@ -3,22 +3,21 @@ package map.homepage.domain.common;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.security.Timestamp;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-@Setter
 public abstract class BaseEntity {
 
     @CreatedDate
-    private LocalDateTime createdAt; // 생성 시간
+    protected LocalDateTime createdAt; // 생성 시간
 
     @LastModifiedDate
-    private LocalDateTime updatedAt; // 수정 시간
+    protected LocalDateTime updatedAt; // 수정 시간
 }
