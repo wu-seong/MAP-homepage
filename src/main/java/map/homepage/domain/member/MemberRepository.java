b2fd3fcaabbad82c183ae3a09b440fb5309de649
wuseong <jws001228@naver.com>
2024-01-24 18:35:58 +0900

    fix: 페이징 메서드 파라미터 PageRequest -> Pageable로 수정
    구현체에 의존하여 나던 버그 인터페이스로 바꾸어 해결 리플렉션 과정에서 오류가 나는 듯?

--- src/main/java/map/homepage/domain/member/MemberRepository.java
+++ src/main/java/map/homepage/domain/member/MemberRepository.java
@@ -3,13 +3,13 @@ package map.homepage.domain.member;
 import map.homepage.domain.member.enums.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByOauthId(Long OauthId);
     Page<Member> findAllByStatus(Status status, PageRequest pageRequest);
-    Page<Member> findAll(PageRequest pageRequest);
+    Page<Member> findAll(Pageable pageRequest);
 }
