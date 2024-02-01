dc5a84140b104089e0c9a1789ad955bbfabd4bbe
kwondoha <ehehgk@naver.com>
2024-02-01 16:45:09 +0900

    feature: AWS S3 셋팅중

---
+++ src/main/java/map/homepage/aws/AmazonS3Manager.java
@@ -0,0 +1,42 @@
+package map.homepage.aws;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import io.jsonwebtoken.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import map.homepage.config.AmazonConfig;
+import map.homepage.domain.photo.Uuid;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AmazonS3Manager{
+
+    private final AmazonS3 amazonS3;
+
+    private final AmazonConfig amazonConfig;
+
+    //private final UuidRepository uuidRepository;
+
+    public String uploadFile(String keyName, MultipartFile file) throws IOException {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        try {
+            amazonS3.putObject(new PutObjectRequest(amazonConfig.getBucket(), keyName, file.getInputStream(), metadata));
+        } catch (IOException e){
+            log.error("error at AmazonS3Manager uploadFile : {}", (Object) e.getStackTrace());
+        } catch (java.io.IOException e) {
+            throw new RuntimeException(e); //뭘까
+        }
+
+        return amazonS3.getUrl(amazonConfig.getBucket(), keyName).toString();
+    }
+
+    public String generatePhotoKeyName(Uuid uuid) {
+        return amazonConfig.getPhotoPath() + '/' + uuid.getUuid();
+    }
+}
