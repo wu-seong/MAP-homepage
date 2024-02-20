d9709794878790a7018af8bd4d57f61dd276ed52
wuseong <jws001228@naver.com>
2024-02-21 00:20:16 +0900

    feat: 관리자 API 컨트롤러 및 서비스 생성

--- src/main/java/map/homepage/domain/member/controller/AdminController.java
+++ src/main/java/map/homepage/domain/member/controller/AdminController.java
@@ -5,11 +5,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import map.homepage.apiPayload.ApiResponse;
 import map.homepage.domain.member.Member;
-import map.homepage.domain.member.auth.MemberContext;
 import map.homepage.domain.member.converter.MemberConverter;
 import map.homepage.domain.member.dto.MemberResponseDTO;
 import map.homepage.domain.member.service.AdminCommandService;
-import map.homepage.domain.member.service.MemberCommandService;
 import map.homepage.domain.member.service.MemberQueryService;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -21,16 +19,29 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
     private final MemberQueryService memberQueryService;
     private final AdminCommandService adminCommandService;
-    @Operation(summary = "유저 목록 조회 API",description = "일반 사용자는 민감하지 않은 기본 정보만, 관리자는 세부정보까지 얻음." +
-            " 근데 이거 나중에 관리자 기능만 따로 빼서 API path 따로 만들듯 ")
+    @Operation(summary = "관리자 전용 유저 목록 조회 API",description = "유저의 세부 정보를 조회합니다.")
     @GetMapping("/members")
     public ApiResponse<MemberResponseDTO.MemberDetailListDTO> getMemberInfos(@RequestParam Integer page){
             Page<Member> MemberPage = memberQueryService.getAll(page);
             return ApiResponse.onSuccess(MemberConverter.toMemberDetailListDTO(MemberPage)); //관리자는 세부 정보를 획득
     }
+    @Operation(summary = "관리자 전용 유저 추방 API",description = "지정된 회원을 회원 탈퇴 시킵니다.")
     @DeleteMapping("/members/{member-id}")
     public ApiResponse<String> deleteMember(@PathVariable(value = "member-id") Long memberId){
         adminCommandService.withdrawMember(memberId);
         return ApiResponse.onSuccess(String.valueOf(memberId) + "번 회원 삭제 성공"); //관리자는 세부 정보를 획득
+    }
+
+    @Operation(summary = "관리자 권한 부여 API",description = "일반 회원에게 관리자 권한을 부여합니다.")
+    @PatchMapping("/members/{member-id}/role/admin")
+    public ApiResponse<String> grantAdminRole(@PathVariable(value = "member-id") Long memberId){
+        adminCommandService.grantAdmin(memberId);
+        return ApiResponse.onSuccess(String.valueOf(memberId) + "번 회원 관리자 권한 부여");
+    }
+    @Operation(summary = "관리자 권한 회수 API",description = "지정된 회원의 관리자 권한을 회수합니다.")
+    @PatchMapping("/members/{member-id}/role/user")
+    public ApiResponse<String> depriveAdminRole(@PathVariable(value = "member-id") Long memberId){
+        adminCommandService.depriveAdmin(memberId);
+        return ApiResponse.onSuccess(String.valueOf(memberId) + "번 회원 관리자 권한 회수");
     }
 }
--- src/main/java/map/homepage/domain/member/service/AdminCommandServiceImpl.java
+++ src/main/java/map/homepage/domain/member/service/AdminCommandServiceImpl.java
@@ -7,11 +7,13 @@ import map.homepage.domain.member.Member;
 import map.homepage.domain.member.MemberRepository;
 import map.homepage.exception.GeneralException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class AdminCommandServiceImpl implements AdminCommandService{
     private final MemberQueryService memberQueryService;
     private final MemberRepository memberRepository;
@@ -20,5 +22,17 @@ public class AdminCommandServiceImpl implements AdminCommandService{
     public void withdrawMember(Long memberId) {
         Member member = memberQueryService.getMemberById(memberId);
         memberRepository.delete(member);
+    }
+
+    @Override
+    public void grantAdmin(Long memberId) {
+        Member member = memberQueryService.getMemberById(memberId);
+        member.setRoleAdmin();
+    }
+
+    @Override
+    public void depriveAdmin(Long memberId) {
+        Member member = memberQueryService.getMemberById(memberId);
+        member.setRoleUser();
     }
 }
